6a0b4e4737bd7bc6707674229b771e2ead41dacb
Durkadevi-ece <durkadeviiyappan@gmail.com>
2025-09-25 07:01:42 +0530

    Update studentclass.java

--- src/week_3/studentclass.java
+++ src/week_3/studentclass.java
@@ -4,7 +4,7 @@ public class studentclass {
     public static void main(String[] args) {
         Student s1 = new Student("Goms",23082000,85);
         s1.display("Student_1");
-        Student s2 = new Student("" + "Dinesh",23072004,87);
+        Student s2 = new Student( "Dinesh",23072004,87);
         s2.display("Student_2");
         Student s3 = new Student("Durka",22082006,84);
         s3.display("Student_3");
